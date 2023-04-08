fb82ef0a47010be4cc900096d6c1117af9ea641c
Kevin Nielsen <knielsen24@gmail.com>
2023-04-08 09:12:09 -0700

    configured dotenv & api key

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-// import { RAPID_API_KEY } from "@env";
-
-
+import { API_KEY } from "@env";
 
 export default function useFetch(endpoint, query) {
     const [data, setData] = useState([]);
@@ -13,7 +11,7 @@ export default function useFetch(endpoint, query) {
         method: "GET",
         url: `https://jsearch.p.rapidapi.com/${endpoint}`,
         headers: {
-            "X-RapidAPI-Key":
+            "X-RapidAPI-Key": process.env.API_KEY,
             "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
         },
         params: { ...query },
